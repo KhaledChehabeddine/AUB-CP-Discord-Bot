6e6d8a592f6b898e039216bceb7323aeffb610c2
KhaledChehabeddine <73232013+KhaledChehabeddine@users.noreply.github.com>
2021-05-11 10:41:34 +0300

    Update CodeForces_Ranking.py

--- databases/CodeForces_Ranking.py
+++ databases/CodeForces_Ranking.py
@@ -1,12 +1,12 @@
 DataBase = {
-  "Inactive" : -1e18,
-  "Newbie" : 1200,
-  "Pupil" : 1400,
-  "Specialist" : 1600,
-  "Expert" : 1900,
-  "Candidate Master" : 2200,
-  "Master" : 2300,
-  "International Master" : 2400,
-  "Grandmaster" : 2600,
-  "Legendary Grandmaster" : 1e18
+    "Inactive" : -1e18,
+    "Newbie" : 1200,
+    "Pupil" : 1400,
+    "Specialist" : 1600,
+    "Expert" : 1900,
+    "Candidate Master" : 2200,
+    "Master" : 2300,
+    "International Master" : 2400,
+    "Grandmaster" : 2600,
+    "Legendary Grandmaster" : 1e18
 }
